9d4bc29532a9faf7e47411edf313b2f22c9d6922
LeoDesing <95183849+LeoDesing@users.noreply.github.com>
2021-11-28 12:37:40 -0500

    Add files via upload

---
+++ js/acerca-de.js
@@ -0,0 +1,9 @@
+let caja2 = document.createElement("div");
+let contenido = document.createTextNode("Caja Nueva");
+caja2.appendChild(contenido);
+caja2.setAttribute("class", "caja naranja");
+
+var contenido = document.getElementById("container");
+contenido.appendChild(caja);
+
+
---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let toggle_btn = document.getElementById("toggle-btn");
+
+toggle_btn.addEventListener("click", function () {
+    menu.classList.toggle("toggle-boton");
+})
